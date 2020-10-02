cea9689668b8c1d1a75c02eb9f54a029f73faadc
Jfarizano <juanignaciofarizano@gmail.com>
2020-10-02 20:26:04 -0300

    hecho evaluador 1

--- TP1/src/Eval1.hs
+++ TP1/src/Eval1.hs
@@ -12,19 +12,16 @@ import           Data.Strict.Tuple
 type State = M.Map Variable Int
 
 -- Estado nulo
--- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
--- Completar la definición
 lookfor :: Variable -> State -> Int
-lookfor v s = undefined
+lookfor v s = n where (Just n) = M.lookup v s
 
 -- Cambia el valor de una variable en un estado
--- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update = M.insert
 
 -- Evalua un programa en el estado nulo
 eval :: Comm -> State
@@ -37,11 +34,88 @@ stepCommStar Skip s = s
 stepCommStar c    s = Data.Strict.Tuple.uncurry stepCommStar $ stepComm c s
 
 -- Evalua un paso de un comando en un estado dado
--- Completar la definición
 stepComm :: Comm -> State -> Pair Comm State
-stepComm = undefined
+stepComm Skip s = (Skip :!: s)
+stepComm (Let x e) s = (Skip :!: s'')
+                       where
+                         (n :!: s') = evalExp e s
+                         s'' = update x n s'
+stepComm (Seq c0 c1) s = stepComm c1 s' where (_ :!: s') = stepComm c0 s 
+stepComm (IfThenElse e c0 c1) s = if b 
+                                    then stepComm c0 s'
+                                    else stepComm c1 s'
+                                  where (b :!: s') = evalExp e s
+stepComm w@(While e c) s = if b
+                            then stepComm (Seq c w) s'
+                            else (Skip :!: s')
+                           where (b :!: s') = evalExp e s
 
 -- Evalua una expresion
--- Completar la definición
 evalExp :: Exp a -> State -> Pair a State
-evalExp = undefined
+
+-- Expresiones enteras
+evalExp (Const n) s = (n :!: s)
+evalExp (Var x) s = (n :!: s)
+                    where n = lookfor x s
+evalExp (UMinus e) s = ((-n) :!: s')
+                       where (n :!: s') = evalExp e s
+evalExp (Plus e e') s = (n :!: s'')
+                         where
+                           (n0 :!: s') = evalExp e s
+                           (n1 :!: s'') = evalExp e' s'
+                           n = n0  + n1
+evalExp (Minus e e') s = (n :!: s'')
+                         where
+                           (n0 :!: s') = evalExp e s
+                           (n1 :!: s'') = evalExp e' s'
+                           n = n0 - n1
+evalExp (Times e e') s = (n :!: s'')
+                          where
+                           (n0 :!: s') = evalExp e s
+                           (n1 :!: s'') = evalExp e' s'
+                           n = n0 * n1
+evalExp (Div e e') s = (n :!: s'')
+                         where
+                           (n0 :!: s') = evalExp e s
+                           (n1 :!: s'') = evalExp e' s'
+                           n = div n0 n1
+evalExp (EAssgn x e) s = (n :!: s'')
+                          where (n :!: s') = evalExp e s
+                                s'' = update x n s
+evalExp (ESeq e e') s = (n1 :!: s'')
+                        where
+                          (n0 :!: s') = evalExp e s
+                          (n1 :!: s'') = evalExp e' s'
+
+-- Expresiones booleanas
+evalExp BTrue s = (True :!: s)
+evalExp BFalse s = (False :!: s)
+evalExp (Lt e e') s = (n0 < n1 :!: s'')
+                       where (n0 :!: s') = evalExp e s
+                             (n1 :!: s'') = evalExp e' s'
+evalExp (Gt e e') s = (n0 > n1 :!: s'')
+                       where (n0 :!: s') = evalExp e s
+                             (n1 :!: s'') = evalExp e' s'
+evalExp (Eq e e') s = (n0 == n1 :!: s'')
+                       where (n0 :!: s') = evalExp e s
+                             (n1 :!: s'') = evalExp e' s'
+evalExp (NEq e e') s = (n0 /= n1 :!: s'')
+                        where (n0 :!: s') = evalExp e s
+                              (n1 :!: s'') = evalExp e' s'
+evalExp (And e e') s = (b0 && b1 :!: s'')
+                        where (b0 :!: s') = evalExp e s
+                              (b1 :!: s'') = evalExp e' s'                                                       
+evalExp (Or e e') s = ((b0 || b1) :!: s'')
+                        where (b0 :!: s') = evalExp e s
+                              (b1 :!: s'') = evalExp e' s'
+evalExp (Not e) s = (not b :!: s')
+                    where (b :!: s') = evalExp e s                             
+
+
+
+-- intBinOp :: Exp Int -> (Exp Int -> Exp Int -> Exp Int)
+-- intBinOp = case binOp of
+--             Plus -> (+)
+--             Minus -> (-)
+--             Times -> (*)
+--             Div -> div
--- TP1/src/Parser.hs
+++ TP1/src/Parser.hs
@@ -42,47 +42,122 @@ lis = makeTokenParser
   )
 
 ----------------------------------
+--- Operador para combinar parsers
+--- sin perder entrada
+-----------------------------------
+
+(<->) :: Parser a -> Parser a -> Parser a
+(<->) p p' = (try p) <|> p'
+
+----------------------------------
 --- Parser de expressiones enteras
 -----------------------------------
 
 intexp :: Parser (Exp Int)
-intexp = chainl1 (try intexpConst <|> intexpVar <|> intexpUM) (try intexpPlus <|> intexpMinus)                           
+intexp = chainl1 intexp1 intexpSeq
+
+intexp1 :: Parser (Exp Int)
+intexp1 = intexpAssgn <-> intexp2
+
+intexp2 :: Parser (Exp Int)
+intexp2 = chainl1 intexp3 (intexpPlus <-> intexpMinus)
+
+intexp3 :: Parser (Exp Int)
+intexp3 = chainl1 intexp4 (intexpTimes <-> intexpDiv)
+
+intexp4 :: Parser (Exp Int)
+intexp4 = (parens lis intexp) <-> intexpUM <-> intexpConst <-> intexpVar
+
+intexpAssgn :: Parser (Exp Int)
+intexpAssgn = do v <- identifier lis
+                 reservedOp lis "="
+                 e <- intexp
+                 return (EAssgn v e)
+
+intexpSeq :: Parser (Exp Int -> Exp Int -> Exp Int)
+intexpSeq = do {reservedOp lis ","; return ESeq}
 
 intexpConst :: Parser (Exp Int)
-intexpConst = do n <- natural lis
-                 return (Const (fromInteger n))
+intexpConst = do {n <- natural lis; return (Const (fromInteger n))}
 
 intexpVar :: Parser (Exp Int)
-intexpVar = do v <- identifier lis
-               return (Var v)
+intexpVar = do {v <- identifier lis; return (Var v)}
 
 intexpUM :: Parser (Exp Int)
-intexpUM = do reservedOp lis "-"
-              e <- intexp
-              return (UMinus e)
+intexpUM = do {reservedOp lis "-"; e <- intexp4; return (UMinus e)}
 
-intexpTimes :: Parser (Exp Int)
-intexpTimes = do {reservedOp lis "+"; return (\e e' -> (Plus e e'))}
+intexpTimes :: Parser (Exp Int -> Exp Int -> Exp Int)
+intexpTimes = do {reservedOp lis "*"; return Times}
 
-intexpDiv :: Parser (Exp Int)
-intexpDiv = do {reservedOp lis "/"; return (\e e' -> (Div e e'))}
+intexpDiv :: Parser (Exp Int -> Exp Int -> Exp Int)
+intexpDiv = do {reservedOp lis "/"; return Div}
 
 intexpPlus :: Parser (Exp Int -> Exp Int -> Exp Int)
-intexpPlus = do {reservedOp lis "+"; return (\e e' -> (Plus e e'))}
+intexpPlus = do {reservedOp lis "+"; return Plus}
 
 intexpMinus :: Parser (Exp Int -> Exp Int -> Exp Int)
-intexpMinus = do {reservedOp lis "-"; return (\e e' -> (Minus e e'))}
+intexpMinus = do {reservedOp lis "-"; return Minus}
                                     
 -----------------------------------
 --- Parser de expressiones booleanas
 ------------------------------------
 
 boolexp :: Parser (Exp Bool)
-boolexp = return BTrue
+boolexp = chainl1 boolexp1 boolexpOr
+
+boolexp1 :: Parser (Exp Bool)
+boolexp1 = chainl1 boolexp2 boolexpAnd
+
+boolexp2 :: Parser (Exp Bool)
+boolexp2 = boolexpNot <-> boolexp3
+
+boolexp3 :: Parser (Exp Bool)
+boolexp3 = (parens lis boolexp) <-> boolexpBool <-> boolexpOp
+
+boolexpBool :: Parser (Exp Bool)
+boolexpBool = do reserved lis "true"
+                 return BTrue
+                 <-> (do reserved lis "false"
+                         return BFalse)
+
+boolexpNot :: Parser (Exp Bool)
+boolexpNot = do reservedOp lis "!"
+                b <- boolexp2
+                return (Not b)
+
+boolexpOp :: Parser (Exp Bool)
+boolexpOp = do e <- intexp
+               op <- (try boolexpEq <-> boolexpNEq <-> boolexpGt <-> boolexpLt)
+               e' <- intexp
+               return (op e e')
+
+boolexpOr :: Parser (Exp Bool -> Exp Bool -> Exp Bool)
+boolexpOr = do {reservedOp lis "||"; return Or}
+
+boolexpAnd :: Parser (Exp Bool -> Exp Bool -> Exp Bool)
+boolexpAnd = do {reservedOp lis "&&"; return And}
+
+boolexpEq :: Parser (Exp Int -> Exp Int -> Exp Bool)
+boolexpEq = do {reservedOp lis "=="; return Eq}
+
+boolexpNEq :: Parser (Exp Int -> Exp Int -> Exp Bool)
+boolexpNEq = do {reservedOp lis "!="; return NEq}
+
+boolexpGt :: Parser (Exp Int -> Exp Int -> Exp Bool)
+boolexpGt = do {reservedOp lis ">"; return Gt}
+
+boolexpLt :: Parser (Exp Int -> Exp Int -> Exp Bool)
+boolexpLt = do {reservedOp lis "<"; return Lt}
 
 -----------------------------------
 --- Parser de comandos
 -----------------------------------
+
+comm :: Parser Comm
+comm = commSeq
+
+comm1 :: Parser Comm
+comm1 = commVar <-> commIf <-> commWhile <-> commSkip
 
 commSkip :: Parser Comm
 commSkip = do reserved lis "skip"
@@ -95,29 +170,22 @@ commVar = do v <- identifier lis
              return (Let v e)
 
 commSeq :: Parser Comm
-commSeq = chainl1 comm (do {reservedOp lis ";"; return (\c c' -> (Seq c c'))})
+commSeq = chainl1 comm1 (do {reservedOp lis ";"; return Seq})
 
 commIf :: Parser Comm
 commIf = do reserved lis "if"
             b <- boolexp
-            char '{'
-            c <- comm
-            char '}'
-            (do try (do reserved lis "else"
-                        char '{'
-                        c' <- comm
-                        char '}'
-                        return (IfThenElse b c c'))
-                <|> return (IfThen b c))
+            c <- braces lis comm
+            (do reserved lis "else"
+                c' <- braces lis comm
+                return (IfThenElse b c c')
+                <-> return (IfThen b c))
 
-comm :: Parser Comm
-comm = do commSkip
-          <->
-          commVar
-          <->
-          commIf
-          <->
-          commSeq
+commWhile :: Parser Comm
+commWhile = do reserved lis "while"
+               b <- boolexp
+               c <- braces lis comm
+               return (While b c)
 
 ------------------------------------
 -- Función de parseo
@@ -125,6 +193,3 @@ comm = do commSkip
 
 parseComm :: SourceName -> String -> Either ParseError Comm
 parseComm = parse (totParser comm)
-
-(<->) :: Parser a -> Parser a -> Parser a
-(<->) x y = (try x) <|> y
